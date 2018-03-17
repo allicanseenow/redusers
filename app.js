dcf0ce5c1752f1734a7cd4c152548b6e8979d799
Amakawa <firesidecake@gmail.com>
2018-03-17 23:14:03 +1100

    Finish the sample Redis app

--- app.js
+++ app.js
@@ -5,6 +5,13 @@ const bodyParser= require('body-parser');
 const methodOverride = require('method-override');
 const redis = require('redis');
 
+// Create Redis clients
+let client = redis.createClient();
+
+client.on('connect', () => {
+  console.log('Redis connected');
+});
+
 // Set Port
 const port = 3000;
 
@@ -23,10 +30,64 @@ app.use(bodyParser.urlencoded({extended: false}));
 // methodOverride: allows DELETE request
 app.use(methodOverride('_method'));
 
+// To use CSS files, ('static files')
+app.use(express.static(path.join(__dirname, 'views')));
+
+// Search page
 app.get('/', (req, res, next) => {
   res.render('searchusers');
 });
 
+// Search processing
+// This is a POST request, instead of GET, so that we can use req.body instead of req.params
+app.post('/user/search', (req, res, next) => {
+  let id = req.body.id;
+
+  client.hgetall(id, (err, obj) => {
+    if (!obj) {
+      res.render('searchusers', {
+        error: 'User does not exist'
+      });
+    } else {
+      obj.id = id;
+      res.render('details', {
+        user: obj,
+      });
+    }
+  });
+});
+
+// Add user page
+app.get('/user/add', (req, res, next) => {
+  res.render('adduser');
+});
+
+// Process Add User page
+app.post('/user/add', (req, res, next) => {
+  let { id, first_name, last_name, email, phone } = req.body;
+  console.log('req body inside user/add/', req.body);
+
+  client.hmset(id, [
+    'first_name', first_name,
+    'last_name', last_name,
+    'email', email,
+    'phone', phone,
+  ], (err, reply) => {
+    if (err) {
+      console.log("Error is ", err);
+    }
+    console.log(reply);
+    res.redirect('/');
+  });
+});
+
+// Delete User
+app.delete('/user/delete/:id', (req, res, next) => {
+  client.del(req.params.id);
+  console.log("successfull deleted client id ", req.params.id);
+  res.redirect('/');
+});
+
 app.listen(port, () => {
   console.log('server started on port ', port);
-})
+});
